b4647e81f4a080461523e09f59fd24cae0f33f53
Patrick Bechon <patrick@bechon.eu>
2022-01-28 22:43:55 +0100

    Dirty fix for an inverted display

--- src/KerbalNavball.cpp
+++ src/KerbalNavball.cpp
@@ -325,7 +325,7 @@ Point2D KerbalNavball::getXY(float lat, float lon){
   Point3D pt2 = {pt1.x*cos_d(_yaw)+pt1.z*sin_d(-_yaw), pt1.y, -pt1.x*sin_d(-_yaw)+pt1.z*cos_d(_yaw)};
 
   // Apply pitch rotation
-  Point3D pt3 = {pt2.x, pt2.y*cos_d(_pitch)+pt2.z*sin_d(_pitch), -pt2.y*sin_d(_pitch)+pt2.z*cos_d(_pitch)};
+  Point3D pt3 = {pt2.x, pt2.y*cos_d(_pitch)+pt2.z*sin_d(-_pitch), -pt2.y*sin_d(-_pitch)+pt2.z*cos_d(_pitch)};
 
   // Apply roll rotation
   Point3D pt4 = {pt3.x*cos_d(_roll)+pt3.y*sin_d(_roll), -pt3.x*sin_d(_roll)+pt3.y*cos_d(_roll), pt3.z};
